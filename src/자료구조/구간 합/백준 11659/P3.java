2631b62aa0086fdcdf8263508890920737a567df
ksr0818 <ksr940818@gmail.com>
2023-07-13 22:54:09 +0900

    Add files via upload

---
+++ src/자료구조/구간 합/백준 11659/P3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class P3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numOfData = sc.nextInt();
+        int numOfQuestion = sc.nextInt();
+
+        int[] data = new int[numOfData + 1];
+        int[] sumOfData = new int[numOfData + 1];
+
+        sumOfData[0] = 0;
+        data[0] = 0;
+
+
+        for (int i=1; i <=numOfData; i++) {
+            data[i] = sc.nextInt();
+
+            sumOfData[i] = sumOfData[i-1] + data[i];
+        }
+
+        for (int i=0; i < numOfQuestion; i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+
+            System.out.println(sumOfData[end] - sumOfData[start-1]);
+        }
+    }
+}
